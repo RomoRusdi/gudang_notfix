042615b087001c7a04a20373f86259c25dc7f383
Romeoraffael <romeoraffael24@gmail.com>
2026-04-27 19:37:25 +0700

    initial commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig, env } from 'prisma/config'
-import 'dotenv/config'
+import { defineConfig, env } from '@prisma/config';
+import 'dotenv/config';
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
-  migrations: {
-    path: './prisma/migrations',
-    seed: 'tsx prisma/seed.ts',
-  },
+  schema: 'prisma/schema.prisma',
   datasource: {
     url: env('DATABASE_URL'),
+    directUrl: env('DIRECT_URL')
   },
-})
+  migrations: {
+    // Kita gunakan tsx (TypeScript Execute) untuk menjalankan file .ts
+    seed: 'npx tsx prisma/seed.ts'
+  }
+});
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,3 +1,4 @@
+"use server";
 import { prisma } from '../db'
 import { createServerFn } from '@tanstack/react-start'
 
